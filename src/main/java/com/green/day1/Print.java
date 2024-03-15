919dc99cb52bad65536ed4f43d4ebbc89b7ec62e
firstt <khi05040@naver.com>
2024-03-15 12:47:23 +0900

    first

---
+++ src/main/java/com/green/day1/Print.java
@@ -0,0 +1,15 @@
+package com.green.day1;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("Hello!도레미\n");
+        System.out.print("Hello!\n");
+        System.out.print("Hello!\n");
+
+        System.out.println("안녕!");
+        System.out.println("안녕!");
+        System.out.println("안녕!");
+        System.out.println("안녕!");
+
+    }
+}
---
+++ src/main/java/com/green/day2/CompOperator.java
@@ -0,0 +1,25 @@
+package com.green.day2;
+
+//비교연산자
+//comparison operator
+
+public class CompOperator {
+    public static void main(String[] args) {
+        //정수형 데이터 타입인 int형
+        //불린형, true, false 값만 가질 수 있는 데이터 타입
+        boolean b1 = true;
+        boolean b2 = false;
+        //boolean b3 = 32;
+
+        boolean r1 = 10 > 9; //10이 9보다 초과인가?
+        System.out.println(r1);
+
+        r1 = 10 < 9; //10이 9보다 미만인가?
+        System.out.println(r1);
+
+        r1 = 10 >= 9;
+        System.out.println(r1);
+
+
+    }
+}
